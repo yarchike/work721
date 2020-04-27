199484fae199d58a214b42b25da39c86f66c7682
Yarchik <yarchike@gmail.com>
2020-04-27 18:50:02 +0300

    Убраны лищниие переменные

--- app/src/main/java/com/example/work721/MainActivity.java
+++ app/src/main/java/com/example/work721/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btn_find = findViewById(R.id.find_btn);
         address = findViewById(R.id.text_address);
-        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         final Intent intent = new Intent();
         intent.setAction(Intent.ACTION_VIEW);
 
